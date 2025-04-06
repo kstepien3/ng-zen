89cde396a8f3a40224f54c75ce87e48d6999d5fe
Konrad Stępień <kord.stp@gmail.com>
2025-04-06 12:15:59 +0200

    feat(switch): add component
    TODO: style refactor

--- src/schematics/components/components-generator.ts
+++ src/schematics/components/components-generator.ts
@@ -1,6 +1,6 @@
 import { GeneratorSchemaBase } from '../../types';
 
-export type ComponentType = 'avatar' | 'button' | 'checkbox' | 'input' | 'textarea';
+export type ComponentType = 'avatar' | 'button' | 'checkbox' | 'input' | 'switch' | 'textarea';
 
 export interface ComponentGeneratorSchema extends GeneratorSchemaBase {
   components: ComponentType[];
