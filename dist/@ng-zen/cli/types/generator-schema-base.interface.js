be1d3e1929ad6ecc0bd60c02779feb7d087d99f8
Konrad Stępień <kord.stp@gmail.com>
2025-05-24 11:47:35 +0200

    chore(release-config): adjust dist handling and release assets (#192)
    - ignore nested files in /dist/**
    - remove dist/@ng-zen/cli/** from release assets in .releaserc.json

--- dist/@ng-zen/cli/types/generator-schema-base.interface.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=generator-schema-base.interface.js.map
--- dist/@ng-zen/cli/utils/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './apply-file-template.util';
