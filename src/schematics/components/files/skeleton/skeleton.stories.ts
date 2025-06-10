fa21c9a440ce918d521c12cd9e9eec17cade60cd
Konrad Stępień <kord.stp@gmail.com>
2025-06-10 11:24:27 +0200

    feat(skeleton): add component (#205)

---
+++ src/schematics/components/files/skeleton/skeleton.stories.ts
@@ -0,0 +1,37 @@
+import { Meta, StoryObj } from '@storybook/angular';
+
+import { ZenSkeletonComponent } from './skeleton.component';
+
+interface StoryParams {
+  rounded: boolean;
+  height: number;
+  width: number;
+}
+
+type StoryType = ZenSkeletonComponent & StoryParams;
+
+export default {
+  title: 'Components/Skeleton',
+  component: ZenSkeletonComponent,
+  tags: ['autodocs'],
+  argTypes: {
+    rounded: { control: { type: 'boolean' } },
+    height: { control: { type: 'range', min: 1, max: 20, step: 0.25 }, description: 'Height managed by css' },
+    width: { control: { type: 'range', min: 1, max: 20, step: 0.25 }, description: 'Width managed by css' },
+  },
+  args: {
+    rounded: false,
+    height: 3,
+    width: 3,
+  },
+} satisfies Meta<StoryType>;
+
+type Story = StoryObj<StoryType>;
+
+export const Default: Story = {
+  render: args => ({
+    props: { ...args },
+    template: `
+      <zen-skeleton ${args.rounded ? 'rounded' : ''} style="width: ${args.width}rem; height: ${args.height}rem"/>`,
+  }),
+};
