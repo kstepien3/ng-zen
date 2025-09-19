b09f02aa409bf1eb8755e8d97ea5699e6af7d22f
Konrad Stępień <kord.stp@gmail.com>
2025-09-19 07:06:42 +0200

    docs(storybook): add brand theme (#325)

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,13 +1,16 @@
 import { setCompodocJson } from '@storybook/addon-docs/angular';
 import type { Preview } from '@storybook/angular';
+import { themes } from 'storybook/theming';
 
 import docJson from '../documentation/documentation.json';
+
 setCompodocJson(docJson);
 
 const preview: Preview = {
   parameters: {
     layout: 'centered',
     docs: {
+      theme: themes.light,
       codePanel: true,
     },
   },
---
+++ .storybook/themes/base.ts
@@ -0,0 +1,10 @@
+export const brand = {
+  // Brand configuration
+  brandTitle: 'NG-Zen UI',
+  brandUrl: 'https://kstepien3.github.io/ng-zen',
+  brandTarget: '_self',
+
+  // Shared properties like typography
+  fontBase: '"Segoe UI", Tahoma, Geneva, Verdana, sans-serif',
+  fontCode: 'Monaco, "Lucida Console", "Courier New", monospace',
+};
