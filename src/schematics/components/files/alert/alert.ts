bc59953dc263561b0a8fb4e3fd0e753c1c14df77
Konrad Stępień <kord.stp@gmail.com>
2025-10-01 18:52:56 +0200

    feat(alert): add component (#337)

---
+++ src/schematics/components/files/alert/alert.ts
@@ -0,0 +1,46 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+/**
+ * ZenAlert is a reusable alert component designed to provide
+ * a consistent and customizable alert style across the application.
+ * It can be used to display messages with different severity levels.
+ *
+ * @example
+ *
+ * ```html
+ * <zen-alert>
+ *   <h3 alert-title>Title</h3>
+ *   content
+ * </zen-alert>
+ * ```
+ *
+ * ### CSS Custom Properties
+ *
+ * You can customize the component using CSS custom properties:
+ *
+ * ```css
+ * :root {
+ *   --zen-alert-padding: 1rem;
+ *   --zen-alert-bg-color: hsl(0deg 0% 100%);
+ *   --zen-alert-color: hsl(0deg 0% 0%);
+ *   --zen-alert-border: 1px solid hsl(0deg 0% 80%);
+ * }
+ * ```
+ *
+ * @author Konrad Stępień
+ * @license {@link https://github.com/kstepien3/ng-zen/blob/master/LICENSE|BSD-2-Clause}
+ * @see [GitHub](https://github.com/kstepien3/ng-zen)
+ */
+@Component({
+  selector: 'zen-alert',
+  template: `
+    <ng-content select="zen-icon" />
+    <div class="content">
+      <ng-content select="[alert-title]" />
+      <ng-content />
+    </div>
+  `,
+  styleUrl: './alert.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ZenAlert {}
