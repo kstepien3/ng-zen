7b821ca49e03a6d0e81d345e78c53a81e1e5a74a
Konrad Stępień <kord.stp@gmail.com>
2025-10-01 19:15:53 +0200

    docs(seo): fix base path

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,12 @@ import type { StorybookConfig } from '@storybook/angular';
 const config: StorybookConfig = {
   stories: ['./stories/**/*.@(js|jsx|ts|tsx|mdx)', '../src/schematics/**/*.stories.@(ts)'],
   addons: ['@storybook/addon-links', '@chromatic-com/storybook', '@storybook/addon-docs'],
-  framework: '@storybook/angular',
+  framework: {
+    name: '@storybook/angular',
+    options: {
+      base: '/ng-zen/',
+    },
+  },
   staticDirs: ['../assets/'],
   docs: {
     defaultName: 'Docs',
