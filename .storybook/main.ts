3afc12595d39ae8ffad8223e787862d143ec9b6a
Konrad Stępień <kord.stp@gmail.com>
2025-10-02 07:09:30 +0200

    docs(seo): add seo features (#332)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,8 +3,13 @@ import type { StorybookConfig } from '@storybook/angular';
 const config: StorybookConfig = {
   stories: ['./stories/**/*.@(js|jsx|ts|tsx|mdx)', '../src/schematics/**/*.stories.@(ts)'],
   addons: ['@storybook/addon-links', '@chromatic-com/storybook', '@storybook/addon-docs'],
-  framework: '@storybook/angular',
-  staticDirs: ['../images'],
+  framework: {
+    name: '@storybook/angular',
+    options: {
+      base: '/ng-zen/',
+    },
+  },
+  staticDirs: ['../assets/'],
   docs: {
     defaultName: 'Docs',
   },
